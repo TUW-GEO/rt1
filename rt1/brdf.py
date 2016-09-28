10878423df27af321ec8f64ec1a257f0d530e4da
Alex Loew <a.loew@gmx.net>
2016-09-28 08:29:43 +0200

    further coding

--- rt1/brdf.py
+++ rt1/brdf.py
@@ -12,15 +12,13 @@ class Surface(object):
     def __init__(self, **kwargs):
         pass
 
-    def brdf(self, mu_0, mu_s):
+    def brdf(self, ctheta):
         """
         Calculate BRDF as function of geometry
 
         Parameters
         ----------
-        mu_0 : float
-            cosine of incident angle
-        mu_s : float
+        ctheta : float
             cosine of scattering angle
         """
         assert False, 'This subroutine should be implemented on sub-class level'
@@ -30,14 +28,13 @@ class Isotropic(Surface):
     def __init__(self, **kwargs):
         super(Isotropic, self).__init__(**kwargs)
 
-    def brdf(self, mu_0, mu_s):
+    def brdf(self, ctheta):
         """
         define BRDF function
+
         Parameters
         ----------
-        mu_0 : float
-            cosine of incident angle
-        mu_s : float
+        ctheta : float
             cosine of scattering angle
         """
         return 1./np.pi
---
+++ rt1/phase.py
@@ -0,0 +1,54 @@
+"""
+Definition of volume phase scattering functions
+"""
+
+class Volume(object):
+    def __init__(self, **kwargs):
+        self.omega = kwargs.pop('omega', None)
+        assert self.omega is not None, 'Single scattering albedo needs to be provided'
+
+        self.tau = kwargs.pop('tau', None)
+        assert self.tau is not None, 'Optical depth needs to be provided'
+
+        assert self.omega >= 0.
+        assert self.tau >= 0.
+
+    def p(self, mu_0, mu_s):
+        """
+        phase function wrapper
+
+        Parameters
+        ----------
+        ctheta : float
+            cosine of scattering angle
+        """
+        assert False, 'phase function to be defined in sub-classes'
+
+
+
+
+
+
+
+class Rayleigh(Volume):
+    """
+    class to define Rayleigh scattering
+    """
+    def __init__(self, **kwargs):
+        super(Rayleigh, self).__init__(**kwargs)
+
+    def p(self, ctheta):
+        """
+        Parameters
+        ----------
+        ctheta : float
+            cosine of scattering angle
+        """
+        # calculate cosine of scattering angle
+        return (3./(16.*np.pi)) * (1. + ctheta**2.)
+
+
+
+
+
+
