d789a73746741a91abd82dc2228e940240dcbdbe
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2016-11-08 17:13:13 +0100

    numerical evaluation test-file uploaded
    the code uses a simple simpson-integration rule to evaluate the integral of the interaction-term numerically.

---
+++ doc/numerical_evaluation.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 18:22:59 2016
+
+@author: rquast
+"""
+
+import numpy as np
+from scipy.integrate import simps
+
+"""
+ the used method is adapted from:
+ http://stackoverflow.com/questions/20668689/integrating-2d-samples-on-a-rectangular-grid-using-scipy
+"""
+
+# phase-function definitions in terms of spherical coordinate system angles (NOT zenith-angles)
+def HG(thetai,thetas,phii,phis,t):
+    return 1./(4.*np.pi)*(1.-t**2)/(1.+t**2 - 2.*t*(np.cos(thetai)*np.cos(thetas)+np.sin(thetai)*np.sin(thetas)*np.cos(phii-phis)))**(3./2.)
+
+def RAYLEIGH(thetai,thetas,phii,phis):
+    return (3./(16.*np.pi)*(1.+(-np.cos(thetai)*np.cos(thetas)+np.sin(thetai)*np.sin(thetas)*np.cos(phii-phis))**2))
+
+def COSLOBE(thetai,thetas,phii,phis,t):
+    asdf=np.maximum((-np.cos(thetai)*np.cos(thetas)+np.sin(thetai)*np.sin(thetas)*np.cos(phii-phis))**t,0.)
+    return asdf
+
+            
+# parameters for example 1 and 2 of the paper:
+            
+CosP=5.                     # set the power of the cosine-lobe     
+HGt=0.7                     # set the asymmetry parameter of the Henyey Greenstein Function
+phii = 0.                   # set the incident direction to phi_i = 0.
+
+omega = 0.3
+tau = 0.7
+
+
+
+# set incident directions for which the integral should be evaluated
+# notice that this is equal to the zenit-angle theta_0 since the arguments in the paper are theta_0 !
+incnum = np.arange(0.,90.,10.)
+
+# define grid for integration 
+x=np.linspace(0.,np.pi/2.,3000)
+y=np.linspace(0.,2*np.pi,3000)
+
+# initialize array for solutions
+solCosRay = []
+solCosHG = []
+
+
+# ---- evaluation of first example
+print('start of evaluation of first example')
+for thetai in np.deg2rad(incnum):
+    # define the function that has to be integrated (i.e. Eq.20 in the paper)
+    # notice the additional  np.sin(thetas)  which oritinates from integrating over theta_s instead of mu_s
+    mu0 = np.cos(thetai)
+    def integfunkt(thetas,phis):
+        return np.array(np.sin(thetas)*2.*omega*np.exp(-tau/mu0)*mu0*np.cos(thetas)/(mu0-np.cos(thetas)) * (np.exp(-tau/mu0)-np.exp(-tau/np.cos(thetas))) *  HG(thetai,thetas,phii,phis,HGt)*COSLOBE(thetai,np.pi-thetas,np.pi,phis,CosP))
+
+    # evaluate the integral using Simpson's Rule twice
+    z=integfunkt(x[:,None],y)
+    solCosRay = solCosRay + [simps(simps(z,y),x)]
+
+solCosRay = np.array(solCosRay)
+
+
+
+# ---- evaluation of second example
+print('start of evaluation of second example')
+for thetai in np.deg2rad(incnum):
+    # define the function that has to be integrated (i.e. Eq.20 in the paper)
+    # notice the additional  np.sin(thetas)  which oritinates from integrating over theta_s instead of mu_s
+    mu0 = np.cos(thetai)
+    def integfunkt(thetas,phis):
+        return np.array(np.sin(thetas)*2.*omega*np.exp(-tau/mu0)*mu0*np.cos(thetas)/(mu0-np.cos(thetas)) * (np.exp(-tau/mu0)-np.exp(-tau/np.cos(thetas))) *  RAYLEIGH(thetai,thetas,phii,phis)*COSLOBE(thetai,np.pi-thetas,np.pi,phis,CosP))
+
+    # evaluate the integral using Simpson's Rule twice
+    z=integfunkt(x[:,None],y)
+    solCosHG = solCosHG + [simps(simps(z,y),x)]
+
+solCosHG = np.array(solCosHG)
+
+
+
+fig = plt.figure()
+axnum = fig.add_subplot(1,1,1)
+
+# plot of numerical results
+axnum.plot(incnum,solCosRay, 'bo')
+axnum.plot(incnum,solCosHG, 'bo')
+
+# plot of result from model calculation 
+#  !!!!!! examples.py needs to be run first to assign inc and Iint
+axnum.plot(inc,Iint)
+
+
+plt.show()
+
