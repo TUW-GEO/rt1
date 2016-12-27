4c7523655234d2ab8cacceda2231dff745a5b1f4
Alex Loew <a.loew@gmx.net>
2016-12-27 15:23:50 +0100

    corrected test, now running

--- tests/test_surface.py
+++ tests/test_surface.py
@@ -52,10 +52,6 @@ class TestSurface(unittest.TestCase):
             z2 = sc.gamma((7.-i)*0.5)*sc.gamma((8.+i)*0.5)
             self.assertAlmostEqual(S._get_legcoef(i), z1/z2)
 
-
-
-
-
         self.assertAlmostEqual(S._get_legcoef(0), 15.*np.sqrt(np.pi)/(16.*sc.gamma(3.5)*sc.gamma(4.)))
         self.assertAlmostEqual(S._get_legcoef(2), 75.*np.sqrt(np.pi)/(16.*sc.gamma(2.5)*sc.gamma(5.)))
 
@@ -66,27 +62,25 @@ class TestSurface(unittest.TestCase):
         phi_s = 0.
 
         # reference solution based on first N Legrende polynomials
-        S = CosineLobe(ncoefs=2, i=5)   # means coefficients 0...9; i=5 is for the example in the paper
+        ncoefs = 10
+        S = CosineLobe(ncoefs=ncoefs, i=5)   # means coefficients 0...9; i=5 is for the example in the paper
 
-        # input parameters are set in a way that COS_THETA = 0
+        # input parameters are set in a way that COS_THETA = 1
         # and therefore only the legendre coefficients should be returned
+        # which is always 1 in case that cos_THETA=1
         theta_i = 0.1234
         theta_s = np.pi/2.
         phi_i = 0.3456
-        phi_s = np.pi/2.
+        phi_s = 0.
 
         r = S._eval_legpoly(theta_i,theta_s,phi_i,phi_s, geometry='ffff')
-        ref = S._get_legcoef(0)*1. + S._get_legcoef(1)*0. #+ S._get_legcoef(2)*(-0.5)  + S._get_legcoef(4)*(3./8.) + S._get_legcoef(6)*(-5./16.) + S._get_legcoef(8) * (35./128.) #+ S._get_legcoef(10)*(-63./256.)
+        #ref = S._get_legcoef(0)*1. + S._get_legcoef(1)*1. #+ S._get_legcoef(2)*(-0.5)  #+ S._get_legcoef(4)*(3./8.) + S._get_legcoef(6)*(-5./16.) + S._get_legcoef(8) * (35./128.) #+ S._get_legcoef(10)*(-63./256.)
 
-        #~ refs = []
-        #~ refs.append(S._get_legcoef(0)*1.)
-        #~ refs.append(S._get_legcoef(2)*(-0.5))
-        #~ refs.append(S._get_legcoef(4)*(3./8.))
-        #~ refs.append(S._get_legcoef(6) * (-5./16.))
-        #~ refs.append(S._get_legcoef(8) * (35./128.))
-        #~ refs.append(S._get_legcoef(10) * (-63./256.))
-        #~ ref = np.array(refs).sum()
-
+        # calculate reference solution
+        refs = []
+        for k in xrange(ncoefs):
+            refs.append(S._get_legcoef(k)*1.)
+        ref = np.array(refs).sum()
         self.assertAlmostEqual(r, ref, 15)
 
     @nottest  # does not work properly yet!
