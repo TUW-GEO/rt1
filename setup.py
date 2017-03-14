f62755469a3950328e1c7b7901401ea3cadd5d5f
Alex <alexander.loew@lmu.de>
2017-03-14 18:35:44 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -49,18 +49,12 @@ def get_packages():
 
 setup(name='rt1',
 
-      version='0.1.1',
+      version='0.1.2',
 
       description='rt1 - bistatic single scattering radiative transfer model',
 
-      # You can just specify the packages manually here if your project is
-      # simple. Or you can use find_packages().
-      # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
       packages=get_packages(),
       package_dir={'rt1': 'rt1'},
-      #~ package_data={'pycmbs': ['benchmarking/configuration/*',
-                               #~ 'benchmarking/logo/*', 'version.json']},
 
       author="Alexander Loew",
       author_email='alexander.loew@lmu.de',
@@ -72,24 +66,9 @@ setup(name='rt1',
       url='https://github.com/pygeo/rt1',
 
       long_description='xxxx',
-
-      # List run-time dependencies here. These will be installed by pip when your
-      # project is installed. For an analysis of "install_requires" vs pip's
-      # requirements files see:
-      # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
       install_requires=install_requires,
 
       keywords=["physics", "radiative transfer"],
-
-      # To provide executable scripts, use entry points in preference to the
-      # "scripts" keyword. Entry points provide cross-platform support and allow
-      # pip to create the appropriate form of executable for the target
-      # platform.
-
-      #~ entry_points={
-          #~ 'console_scripts': [
-              #~ 'pycmbs_benchmarking = pycmbs_benchmarking:main'
-          #~ ]},
 
       # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -109,14 +88,6 @@ setup(name='rt1',
           # that you indicate whether you support Python 2, Python 3 or both.
           'Programming Language :: Python :: 2.7'
       ],
-
-      #~ ext_modules=[ext_polygon_utils],
-      #~ cmdclass={'build_ext': build_ext}
-
-    #~ ext_modules=cythonize(
-        #~ ["./geoval/polygon/polygon_utils.pyx"]),
-    # this is needed to get proper information on numpy headers
-    #~ include_dirs=[np.get_include()]
 
       )
 
