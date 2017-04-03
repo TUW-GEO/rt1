7a24bebde1c4be161a1e8b1daae2f917e07bb9fa
Raphael Quast <rquast@geo.tuwien.ac.at>
2017-04-03 16:50:55 +0200

    minor to satisfy sticker-ci

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,6 @@ def setup(app):
     app.add_stylesheet('my_theme.css')
 
 
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
