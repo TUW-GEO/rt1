0889480620de74d965bc1e3adbba60e446c9ec52
Raphael Quast <rquast@geo.tuwien.ac.at>
2017-04-06 16:24:17 +0200

    making sticker-ci happy...

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,7 @@ for mod_name in MOCK_MODULES:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..')+os.sep)
+sys.path.insert(0, os.path.abspath('..') + os.sep)
 
 # -- General configuration ------------------------------------------------
 
