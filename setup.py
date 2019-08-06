37451459fd30dbeb7e841fcd7fac29e1801337d2
Raphael <raphael.quast@geo.tuwien.ac.at>
2019-08-06 19:08:15 +0200

    update of setup.py

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ setup(name='rt1',
                         ' covered by a homogeneous layer of scattering media.'
                         ),
 
-      install_requires=["numpy>=1.16", "sympy>=1.4", "scipy>=1.2", "pandas>=0.24"],
-      extras_require={'symengine' : ["symengine>=0.4"],
-                      'plots' : ["matplotlib>=3.0"]},
+      install_requires=["numpy>=1.16", "sympy>=1.4", "scipy>=1.2", "pandas>=0.24",
+                        "matplotlib>=3.0"],
+      extras_require={'symengine' : ["symengine>=0.4"]},
 
       keywords=["physics", "radiative transfer"],
 
