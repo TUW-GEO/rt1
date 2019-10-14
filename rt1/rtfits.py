51f4917783062437279eebf986214de9206b6a74
Raphael <raphael.quast@geo.tuwien.ac.at>
2019-10-14 13:41:22 +0200

    fix of assertion for missing fixed dataset

--- rt1/rtfits.py
+++ rt1/rtfits.py
@@ -1212,10 +1212,11 @@ class Fits(Scatter):
         # (additional columns of 'dataset' do not affect the fit)
 
         for key, val in fixed_dict.items():
-            if key in new_fixed_dict:
-                if isinstance(val, str) and val == 'auxiliary':
+            if isinstance(val, str) and val == 'auxiliary':
                     assert key in new_fixed_dict, \
                         f"auxiliary data for '{key}' is missing!"
+
+            if key in new_fixed_dict:
                 fixed_dict[key] = new_fixed_dict[key]
 
         # check if tau, omega or NormBRDF is given in terms of sympy-symbols
