a14ff25c09d060ecfa6648514576fab2a486c818
Raphael <raphael.quast@geo.tuwien.ac.at>
2020-03-11 10:46:52 +0100

    add test for HGRayleigh and for V-normalization

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -7,9 +7,10 @@ import sys
 
 sys.path.append('..')
 from rt1.volume import Volume
-from rt1.volume import Rayleigh,HenyeyGreenstein
+from rt1.volume import Rayleigh, HenyeyGreenstein, HGRayleigh
 import matplotlib.pyplot as plt
-
+from scipy.integrate import simps
+from itertools import product
 
 class TestVolume(unittest.TestCase):
 
@@ -41,6 +42,23 @@ class TestVolume(unittest.TestCase):
         p = V.p(t_0, t_ex, p_0, p_ex)
         self.assertAlmostEqual(p, (1.-0.7*0.7)/(4.*np.pi*(1.+0.7*0.7)**1.5),10)
 
+    def test_HGRayleigh(self):
+        V = HGRayleigh(omega=0.2, tau=1.7, t=0.7, ncoefs=20)
+        self.assertEqual(V.t, 0.7)
+        t_0 = np.pi/2.
+        t_ex = 0.234234
+        p_0 = np.pi/2.
+        p_ex = 0.
+
+        Vr= Rayleigh(omega=0.2, tau=1.7)
+        Vhg = HenyeyGreenstein(omega=0.2, tau=1.7, t=0.7,ncoefs=20)
+
+        p = V.p(t_0, t_ex, p_0, p_ex)
+        phg = Vhg.p(t_0, t_ex, p_0, p_ex)
+        pr = Vr.p(t_0, t_ex, p_0, p_ex)
+
+        self.assertAlmostEqual(p, 4.*np.pi*(4/(2+.7**2))*1/2*phg*pr,10)
+
     def test_HenyeyGreenstein_coefficients(self):
         V = HenyeyGreenstein(omega=0.2, tau=1.7, t=0.7,ncoefs=20)
         self.assertEqual(V._get_legcoef(0),1./(4.*np.pi))
@@ -51,6 +69,43 @@ class TestVolume(unittest.TestCase):
         V = HenyeyGreenstein(omega=0.2, tau=1.7, t=0.7,ncoefs=20)
         pl = V.polarplot()
         plt.close(pl)
+
+
+    def test_V_normalization(self):
+
+        V1 = HenyeyGreenstein(omega=0.2, tau=1.7, t=0.7, ncoefs=20)
+        V2 = HGRayleigh(omega=0.2, tau=1.7, t=0.7, ncoefs=20)
+        V3 = Rayleigh(omega=0.2, tau=1.7)
+
+        for V in [V1,V2,V3]:
+            # set incident (zenith-angle) directions for which the integral
+            # should be evaluated!
+            incnum = np.linspace(0, np.pi, 5)
+            pincnum = np.linspace(0, 2*np.pi, 5)
+
+            # define grid for integration
+            x = np.linspace(0., np.pi, 500)
+            y = np.linspace(0., 2 * np.pi, 500)
+
+            # initialize array for solutions
+            sol = []
+            # ---- evaluation of Integral
+            # adapted from
+            # (http://stackoverflow.com/questions/20668689/integrating-2d-samples-on-a-rectangular-grid-using-scipy)
+            for theta_0, phi_0 in product(incnum, pincnum):
+                # define the function that has to be integrated
+                # (i.e. Eq.20 in the paper)
+                # notice the additional  np.sin(thetas)  which oritinates from
+                # integrating over theta_s instead of mu_s
+                def integfunkt(theta_s, phi_s):
+                    return np.sin(theta_s) * V.p(theta_0, theta_s,
+                                                 phi_0, phi_s)
+                # evaluate the integral using Simpson's Rule twice
+                z = integfunkt(x[:, None], y)
+                sol += [simps(simps(z, y), x)]
+
+            self.assertTrue(np.allclose(sol, 1.))
+
 
 if __name__ == "__main__":
     unittest.main()
