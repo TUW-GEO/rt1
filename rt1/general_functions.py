e1d9881a0e9bda818de16cd45413dc1147f5314b
Raphael <raphael.quast@geo.tuwien.ac.at>
2020-03-19 17:49:27 +0100

    get rid of weighting mechanism in rtfits - don't use rectangular datasets and weights (instead apply mask and use only meaningful values) - remove newcalc argument in plot.fit_error and plot.scatter (always newcalc) - don't return weighting matrix in general_functions.rectangularize

--- rt1/general_functions.py
+++ rt1/general_functions.py
@@ -8,7 +8,7 @@ import pandas as pd
 import datetime
 from itertools import tee
 
-def rectangularize(array, weights_and_mask=False, dim=None,
+def rectangularize(array, return_mask=False, dim=None,
                    return_masked=False):
     '''
     return a rectangularized version of the input-array by repeating the
@@ -25,8 +25,8 @@ def rectangularize(array, weights_and_mask=False, dim=None,
     ------------
     array: list of lists
            the input-data that is intended to be rectangularized
-    weights_and_mask: bool (default = False)
-                     indicator if weights and mask should be evaluated or not
+    return_mask: bool (default = False)
+              	 indicator if weights and mask should be evaluated or not
     dim: int (default = None)
          the dimension of the rectangularized array
          if None, the shortest length of all sub-lists will be used
@@ -37,10 +37,6 @@ def rectangularize(array, weights_and_mask=False, dim=None,
     ----------
     new_array: array-like
                a rectangularized version of the input-array
-    weights: array-like (only if 'weights_and_mask' is True)
-             a weighting-matrix whose entries are 1/sqrt(number of repetitions)
-             (the square-root is used since this weighting will be applied to
-             a sum of squares)
     mask: array-like (only if 'weights_and_mask' is True)
           a mask indicating the added values
 
@@ -49,30 +45,7 @@ def rectangularize(array, weights_and_mask=False, dim=None,
         # get longest dimension of sub-arrays
         dim  = len(max(array, key=len))
 
-    if weights_and_mask is True:
-        newarray, weights, mask = [], [], []
-        for s in array:
-            adddim = dim - len(s)
-            w = np.full_like(s, 1)
-            m = np.full_like(s, False)
-            if adddim > 0:
-                s = np.append(s, np.full(adddim, s[-1]))
-                w = np.append(w, np.full(adddim, 1/np.sqrt(adddim)))
-                m = np.append(m, np.full(adddim, True))
-            newarray += [s]
-            weights  += [w]
-            mask     += [m]
-
-        newarray = np.array(newarray)
-        weights = np.array(weights)
-        mask = np.array(mask, dtype=bool)
-
-        if return_masked is True:
-            newarray = np.ma.masked_array(newarray, mask)
-
-        return [newarray, weights, mask]
-
-    elif return_masked is True:
+    if return_mask is True or return_masked is True:
         newarray, mask = [], []
         for s in array:
             adddim = dim - len(s)
@@ -83,11 +56,13 @@ def rectangularize(array, weights_and_mask=False, dim=None,
             newarray += [s]
             mask     += [m]
 
-        mask = np.array(mask, dtype=bool)
         newarray = np.array(newarray)
+        mask = np.array(mask, dtype=bool)
 
-        return np.ma.masked_array(newarray, mask)
+        if return_masked is True:
+            newarray = np.ma.masked_array(newarray, mask)
 
+        return [newarray, mask]
     else:
         newarray = []
         for s in array:
